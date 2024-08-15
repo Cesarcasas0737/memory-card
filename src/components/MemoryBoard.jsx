4a51b78df3308468353102bea2e942580bf727fd
cesar <cesarcasas0737@gmail.com>
2024-08-14 18:20:57 -0700

    added new functions to add sound, edited game logic handling,edited html render

--- src/components/MemoryBoard.jsx
+++ src/components/MemoryBoard.jsx
@@ -29,7 +29,6 @@ export default function MemoryBoard({
             [scrambledArray[i], scrambledArray[j]] = [scrambledArray[j], scrambledArray[i]];
         }
     
-        console.log(scrambledArray);
         setPokemonsArray(scrambledArray); 
     } 
 
@@ -64,7 +63,6 @@ export default function MemoryBoard({
 
     function launchGameOver(){
         resetAmountOfPoke()
-        
         clearUniqueSet()
         resetCounter()
         negateGameOverStatus()
@@ -75,23 +73,36 @@ export default function MemoryBoard({
         negateGameOverStatus()
     }
 
+    function playSound(soundName){
+        const audio = new Audio(`/sounds/${soundName}.mp3`);
+        audio.play();
+    }
+
     useEffect(() =>{
+
+        //clicked duplicate
         if (uniqueId.size != pokeClickedCount) {  
             //console.log('you clicked a duplicate')
+            playSound('errorClick')
             setScore(score -1)
             if (score === highScore) {
                 setHighScore(highScore -1)
             }
-
             launchGameOver()
         }else{
+        //clicked non-duplicate
+            if(score >=1){
+                playSound('rewardSound')
+            }
             if(score > highScore){
                 setHighScore(score)
             }
             scramblePokeArrayOrder()
         }
 
+        // x/x clicked therefore next lv
         if((pokemonsArray.length === uniqueId.size)){
+            playSound('nextlv')
             pokeListNextLv()
         }
     }, [uniqueId])
@@ -99,11 +110,17 @@ export default function MemoryBoard({
     return(
         <>
             {isNotGameOver ? (
-                <pre>
-                    <h1>{pokeClickedCount}/{pokemonsArray.length}</h1>
-                    <h2>Score {score}</h2>
-                    <h2>High Score {highScore}</h2>  
-                    <MemoryCards
+                <pre className="memoryboard">
+                    <div className="gameUi">
+                        <button onClick={pokeListNextLv}>Next level</button>
+                        <h1>{pokeClickedCount}/{pokemonsArray.length}</h1>
+                        <h2>Score: {score}</h2>
+                        <h2>High Score: {highScore}</h2>  
+                    </div>
+                     
+
+                    <div className="playboard">
+                       <MemoryCards
                         pokemonsArray = {pokemonsArray}
 
                         uniqueId = {uniqueId}
@@ -114,16 +131,18 @@ export default function MemoryBoard({
                         
                         score={score}
                         setScore={setScore}
-                    />
+                        /> 
+                    </div>
+                    
                 </pre>
                 
                 ) : (
                     
-                <div>
+                <div className="gameOverScreen">
                    <h2>Game Over</h2>
-                   <p>Score:{score}</p>
-                   <p>HighScore:{highScore}</p>
-                   <button onClick={launchGameAgain}>Play Again!</button>
+                   <p>Score: {score}</p>
+                   <p>High Score: {highScore}</p>
+                   <button onClick={launchGameAgain}>Play Again?</button>
                 </div>
             )}
         </>
